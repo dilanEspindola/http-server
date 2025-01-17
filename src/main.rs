4cad4df40974304bfbe4b2e6c247af25a50dbdc4
dilan <dilanespindola30@gmail.com>
2025-01-16 22:28:09 -0500

    feat: sending a new client connection to a new thread

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use http_server::server;
+use std::{net::TcpListener, thread};
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4000")?;
@@ -11,27 +9,9 @@ fn main() -> std::io::Result<()> {
     for stream in listener.incoming() {
         match stream {
             Ok(mut socket) => {
-                let mut buffer = [0; 1024];
-
-                match socket.read(&mut buffer) {
-                    Ok(n) => {
-                        let http_line = String::from_utf8_lossy(&buffer[..n]);
-                        println!("Received {}", http_line);
-                        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello, World!");
-                        match socket.write(response.as_bytes()) {
-                            Ok(n) => {
-                                println!("Response sent {}", n);
-                                socket.shutdown(std::net::Shutdown::Both)?;
-                            }
-                            Err(e) => {
-                                println!("Error: {}", e);
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        println!("Error: {}", e);
-                    }
-                }
+                thread::spawn(move || {
+                    server::handler::handle_client(&mut socket);
+                });
             }
             Err(e) => {
                 eprintln!("Error: {}", e);
