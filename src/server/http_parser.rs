8182db6c008310e0988c1204568381a6fdc2b7ad
dilan <dilanespindola30@gmail.com>
2025-03-04 00:18:27 -0500

    refactoring parser & parsing body(ONLY WORKS FOR JSON)

--- src/server/http_parser.rs
+++ src/server/http_parser.rs
@@ -1,32 +1,50 @@
 use std::{borrow::Cow, collections::HashMap};
 
+use serde_json::Value;
+
 pub struct Request {
     pub method: String,
     pub path: String,
     pub http_version: String,
-    pub header: HashMap<String, String>,
-    pub body: Option<String>,
+    pub headers: HashMap<String, String>,
+    pub body: Option<HashMap<String, Value>>,
 }
 
+// The parser is only able to parse JSON
+// TODO: Add support for other formats
 pub fn parser(request: &Cow<'_, str>) -> Request {
-    let split_request: Vec<&str> = request.split(' ').collect();
-    let main_request_http = &split_request.clone()[0..3];
+    let split_request: Vec<&str> = request.split("\r\n\r\n").collect();
+    let first_part = split_request[0];
+    let body = split_request[1];
+
+    let main_request: Vec<&str> = first_part.split("\r\n").collect();
+    let main_request_splitted: Vec<&str> = main_request.get(0).unwrap().split(" ").collect();
 
     let mut headers = HashMap::new();
-
-    for header in request.split("\r\n").skip(1) {
-        let split_header: Vec<&str> = header.split(':').map(|s| s.trim()).collect();
-
-        for (key, value) in split_header.iter().zip(split_header.iter().skip(1)) {
-            headers.insert(key.to_string(), value.to_string());
-        }
+    for header in main_request.iter().skip(1) {
+        let removed_dots: Vec<&str> = header.split(": ").collect();
+        let key = removed_dots[0];
+        let value = removed_dots[1];
+        headers.insert(key.to_string(), value.to_string());
     }
 
+    let body_parsed = match serde_json::from_str::<serde_json::Value>(&body.to_string()) {
+        Ok(s) => {
+            let hashmap: HashMap<String, Value> =
+                serde_json::from_str(s.to_string().as_str()).unwrap();
+            hashmap
+        }
+        Err(_) => {
+            // Value::Null;
+            panic!("Error parsing body");
+        }
+    };
+
     return Request {
-        method: main_request_http[0].to_string(),
-        path: main_request_http[1].to_string(),
-        http_version: main_request_http[2].to_string(),
-        header: headers,
-        body: None,
+        method: main_request_splitted[0].to_string(),
+        path: main_request_splitted[1].to_string(),
+        http_version: main_request_splitted[2].to_string(),
+        headers: headers,
+        body: Some(body_parsed),
     };
 }
